bf1020f4f484ce5499a6ad4692931188e136ae3b
waliulislamtonmoy <tonmoykhandiu@gmail.com>
2024-08-05 13:36:27 +0600

    upload

--- core/Appblog/urls.py
+++ core/Appblog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import HomePage,PostDetails
+from .views import HomePage,PostDetails,CreatePost,PostDeleteView
 
 
 urlpatterns = [
     path("",HomePage.as_view(),name="home"),
     path("post/<int:pk>/",PostDetails.as_view(),name="postdetails"),
+    path("create/",CreatePost.as_view(),name="create"),
+    path("delete/<int:pk>/",PostDeleteView.as_view(),name="delete"),
 ]
--- core/Appblog/views.py
+++ core/Appblog/views.py
@@ -3,15 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 from . models import Post
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 class HomePage(ListView):
     model=Post
     template_name="appblog/Home.html"
     context_object_name='posts'
+    ordering=["-id"]
     
     
 class PostDetails(DetailView):
     model=Post
     template_name='appblog/Detailsblog.html'
+    
+class CreatePost(LoginRequiredMixin,CreateView):
+    model=Post
+    fields=['title','content']
+    template_name="appblog/Newpost.html"
+    success_url="/"
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post 
+    template_name='appblog/Deleteview.html'
+    success_url="/"
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    
+    
+    
+        
+        
+    
     
